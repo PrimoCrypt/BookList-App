989a41b8dc473860364f564c6c797b48d3721f17
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2024-08-03 19:56:37 +0100

    fix: Fixed the large padding on the right of the navbar on mobile

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 
         <h1 className="cursor-pointer text-xl pt-2 rounded-lg font-semibold box-content text-blue-800 shadow-blue-950 shadow-md px-2 self-center flex">Leo Books</h1>
         </div>
-        <div className="flex gap-3 pr-10">
+        <div className="flex gap-3 pr-0 md:pr-10">
             <a href="#" className=" hover:text-blue-500 active:text-red-500">Contact</a>
             <a href="#" className="hover:text-blue-500 active:text-red-500">About</a>
         </div>
