039e4652215855fc8f6face9758fa16355f15091
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2024-08-03 19:02:59 +0100

    refactor: Changed the data file extension from .ts to .js and modified the book data

--- src/data/data.js
+++ src/data/data.js
@@ -1,32 +1,32 @@
 
 export const bookData = [
     {
-        author: "Anesi Divine",
+        author: "Leo Arthur",
         title: "How to make 1 Billion before 20years of age",
         img: "#",
         id: 1
     },
     {
-        author: "Leo Nesi",
+        author: "Martin Kings",
         title: "How to make it in tech in 2024",
         img: "#",
         id: 2
     },
     {
-        author: "Oshione Star",
+        author: "Longman Starr",
         title: "How to shine brighter than the stars at night",
         img: "#",
         id: 3
     },
     {
-        author: "Leo Nesi",
-        title: "How to make it in tech in 2024",
+        author: "Brooks Dayne",
+        title: "How to Build Muscles while eating once daily",
         img: "#",
         id: 4
     },
     {
-        author: "Oshione Star",
-        title: "How to shine brighter than the stars at night",
+        author: "Lammie Johnson",
+        title: "How to GLow using Just water",
         img: "#",
         id: 5
     }
